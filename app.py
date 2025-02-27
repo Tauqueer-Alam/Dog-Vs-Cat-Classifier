124b57f783ecb2264240327ed974850f6f322810
Tauqueer Alam <tauqueeralam100@gmail.com>
2025-02-28 00:04:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from flask import Flask, render_template, request
 import numpy as np
-import pickle
-import cv2
 import os
+import cv2
+from tensorflow.keras.models import load_model
 
-model = pickle.load(open("model.pkl", "rb"))
+model = load_model("model.h5")
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def form():
     return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
@@ -24,8 +27,8 @@ def predict():
 
     file = request.files['file']
     
-    if file.filename == '':
-        return "No selected file", 400
+    if file.filename == '' or not allowed_file(file.filename):
+        return "Invalid file type", 400
 
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(file_path)
@@ -35,12 +38,10 @@ def predict():
     image = image.astype('float32') / 255.0  
     image = np.expand_dims(image, axis=0) 
 
-    result = model.predict(image)[0][0]
-    
+    result = float(model.predict(image))
     prediction = "Dog" if result > 0.5 else "Cat"
 
     return render_template('predict.html', result=prediction)
 
-
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
